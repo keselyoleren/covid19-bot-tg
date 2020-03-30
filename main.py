9c96a575df2531fe20305a1d3dc1f42638e34323
M Hadi Sasmito <keselyoleren@gmail.com>
2020-03-30 22:41:30 +0700

    '

--- main.py
+++ main.py
@@ -11,26 +11,12 @@ def find_at(msg):
         if '@' in text:
             return text
 
-# def info_covod19():
+
 response = requests.get("https://api.kawalcorona.com/indonesia/").json()
-# for data in response:
-    # print(data['name'])
-# a = response.json()
-# print(response)
-
-
-
-
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Welcome')
-
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, 'lihat update penyebaran virus corona dengan cara kirkm /namawilayah contoh /indonesa')
+    bot.reply_to(message, 'lihat update penyebaran virus corona di setiap daerah  /indonesa /bali /aceh /jambi /jatim /jateng /jabar /jakarta /jogja /papu /riau /sumbar /banten')
 
 @bot.message_handler(commands=['indonesia'])
 def indonesia_info(message):
@@ -38,6 +24,185 @@ def indonesia_info(message):
     heder = "\033 Update covid19 \033 \n \n"
     for data in get_info_covid19:
         bot.reply_to(message, heder + " \n Wilayah :" + data['name'] + "\n positif : " + data['positif']  + "\n Sembuh : " + data['sembuh'] + "\n meninggal :" + data['meninggal'])
+
+
+@bot.message_handler(commands=['jatim'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Jawa Timur":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['jabar'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Jawa Barat":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['jateng'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Jawa Tengah":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['jakarta'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "DKI Jakarta":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['banten'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Banten":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['bali'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Bali":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+
+@bot.message_handler(commands=['jogja'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Daerah Istimewa Yogyakarta":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['papua'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Papua":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['sumbar'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Sumatra Barat":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['lampung'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Lampung":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['aceh'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Aceh":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+
+@bot.message_handler(commands=['riau'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Riau":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+
+
+
+@bot.message_handler(commands=['jambi'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Jambi":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['maluku'])
+def jatim_info(message):
+    res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
+
+    for a in res:
+        for key, val in a.items():
+            if val['Provinsi'] == "Maluku":
+                msg = "  Update covid19  \n \n Wilayah: " + val['Provinsi']  + "\n positif: " + str(val['Kasus_Posi']) + "\n Sembuh : " + str(val['Kasus_Semb']) + "\n Meninggal : " + str(val['Kasus_Meni']) 
+                
+                bot.reply_to(message, msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
--- update.py
+++ update.py
@@ -1,31 +1,25 @@
-import time
-import schedule
 import requests
+import json
 
+res = requests.get('https://api.kawalcorona.com/indonesia/provinsi').json()
 
-def telegram_bot_sendtext(bot_message):
-    print(bot_message)
-    
-    bot_token = '1126681710:AAGmM11NBljhLcER_WoM_q_70m6Vehy1Mr4'
-    bot_chatID = '1126681710'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-    print(send_text)
+for a in res:
+    for key, val in a.items():
+        if val['Provinsi'] == "Jawa Timur":
+            message = " \033 Update covid19 \033 \n \n \n Wilayah " , val['Provinsi']  , "\n positif" , val['Kasus_Posi'] , "\n Sembuh" , val['Kasus_Semb'] , "\n Meninggal" , val['Kasus_Meni'] 
+            print(message)
+            # print("probinsi" + val['Provinsi'])
+            # print("positif" , val['Kasus_Posi'])
+            # print("Sembuh" , val['Kasus_Semb'])
+            # print("Meninggal", val['Kasus_Meni'])
+        # else:
+        #     print('kosong')
+    # print(a['attributes'])
+    # data = a['attributes']
+    # for key,val in data.value():
+    #     print(key,val)
 
-    response = requests.get(send_text)
-    print(response.json())
+# c = {'FID': 32, 'Kode_Provi': 82, 'Provinsi': 'Maluku Utara', 'Kasus_Posi': 1, 'Kasus_Semb': 0, 'Kasus_Meni': 0}
 
-    return response.json()
-
-
-def report():
-    my_balance = 10   ## Replace this number with an API call to fetch your account balance
-    my_message = "Current balance is: {}".format(my_balance)   ## Customize your message
-    telegram_bot_sendtext(my_message)
-
-
-    
-# schedule.every().day.at("19:00").do(report)
-report()
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+# for key, value in c.items():
+#     print(key, value)
